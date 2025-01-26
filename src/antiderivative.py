c5e62bfe36feeefe97a73bc58f2ed9c144e89f4d
Stephan Kulla <git.mail@kulla.me>
2025-01-26 16:38:41 +0100

    feat: Add repeated_antiderivative()

--- src/antiderivative.py
+++ src/antiderivative.py
@@ -8,3 +8,21 @@ def antiderivative(f, a, b, x=x):
     Returns the antiderivative of $f$ from $a$ to $b$.
     """
     return sp.integrate(f, (x, a, b))
+
+
+def repeated_antiderivative(n, f, a, b, x=x):
+    """
+    Returns the nth antiderivative $F_n(b)$ of $f$. $F_n(x)$ is defined as:
+
+    1. $F_0(x) = \\int_a^x f(t) \\mathrm dt$
+    2. $F_n(x) = \\int_a^x F_{n-1}(t) \\mathrm dt$
+    """
+    if n < 0:
+        raise TypeError("n must be greater than or equal to zero")
+
+    result = f
+
+    for i in range(n + 1):
+        result = antiderivative(result, a, b if i == n else x, x=x)
+
+    return result
--- tests/antiderivative_test.py
+++ tests/antiderivative_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import sympy as sp
 
-from src.antiderivative import antiderivative
+from src.antiderivative import antiderivative, repeated_antiderivative
 
 
 class TestAntidervative(unittest.TestCase):
@@ -12,6 +12,19 @@ class TestAntidervative(unittest.TestCase):
         self.assertEqual(antiderivative(x, a, b), (b**2 - a**2) / 2)
         self.assertEqual(antiderivative(x**2, 0, 1), sp.Integer(1) / 3)
         self.assertEqual(antiderivative(sp.exp(x), a, b), sp.exp(b) - sp.exp(a))
+
+
+class TestRepeatedAntiderivative(unittest.TestCase):
+    def test_returns_antiderivative_for_n_equals_zero(self):
+        x, a, b = sp.symbols("x a b")
+
+        self.assertEqual(
+            repeated_antiderivative(0, sp.sin(x), a, b), antiderivative(sp.sin(x), a, b)
+        )
+        self.assertEqual(repeated_antiderivative(0, x, a, b), b**2 / 2 - a**2 / 2)
+        self.assertEqual(
+            repeated_antiderivative(0, sp.exp(x), a, b), antiderivative(sp.exp(x), a, b)
+        )
 
 
 if __name__ == "__main__":
