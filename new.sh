d1f025d56d4d2270ea80462c3a28a064483b6751
Stephan Kulla <git.mail@kulla.me>
2025-01-26 14:31:56 +0100

    feat: Do not add date in the notebook name

--- new.sh
+++ new.sh
@@ -4,23 +4,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 NOTEBOOK_DIR="$SCRIPT_DIR/notebooks"
 NOTEBOOK_TITLE="$@"
 
-YEAR=$(date +%Y)
-MONTH=$(date +%m)
-DAY=$(date +%d)
-
 # Convert the input string to lowercase, replace spaces with hyphens, and trim
 # leading/trailing spaces or hyphens.
 FILENAME=$(echo "$NOTEBOOK_TITLE" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | sed 's/^[ \-]*//;s/[ \-]*$//')
 
 # Define the notebook path
-NOTEBOOK_PATH="$NOTEBOOK_DIR/$YEAR-$MONTH-$DAY-$FILENAME.ipynb"
+NOTEBOOK_PATH="$NOTEBOOK_DIR/$FILENAME.ipynb"
 
 # Create the new jupyter notebook
 cp "$NOTEBOOK_DIR/template.ipynb" "$NOTEBOOK_PATH"
 
 # Replace the placeholders in the notebook
 sed -i "s/NOTEBOOK_TITLE/$NOTEBOOK_TITLE/g" "$NOTEBOOK_PATH"
-sed -i "s/YEAR/$YEAR/g" "$NOTEBOOK_PATH"
+sed -i "s/YEAR/$(date +%Y)/g" "$NOTEBOOK_PATH"
 
 # Open the jupyter notebook
 jupyter lab --notebook-dir="$NOTEBOOK_DIR" "$NOTEBOOK_PATH"
