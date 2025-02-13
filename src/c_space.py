4157a97f862e67a4d184ceb0103ef56808071a35
Stephan Kulla <git.mail@kulla.me>
2025-02-14 00:09:30 +0100

    feat: Add different forms helper functions

--- src/c_space.py
+++ src/c_space.py
@@ -22,3 +22,25 @@ def integral_form(f, a=a, b=b, x=x):
     Create a linear form based on the inner product with a fixed function f.
     """
     return partial(inner_product, f, a=a, b=b, x=x)
+
+
+def integral_forms(f, n, a=0, b=1, x=x):
+    return [integral_form(f.subs({"n": i}), a, b, x) for i in range(n)]
+
+
+def vectors(f, n):
+    return [f.subs({"n": i}) for i in range(n)]
+
+
+def derivative_form(n, x0=0, x=x):
+    def derivative(f, x=x):
+        if n == 0:
+            return f.subs(x, x0)
+        else:
+            return sp.diff(f, x, n).subs(x, x0)
+
+    return derivative
+
+
+def derivative_forms(n, x0=0, x=x):
+    return [derivative_form(i, x0, x) for i in range(n)]
