6d5c9995d2acfd7d78eb4b9209ca7f22aaff5164
Stephan Kulla <git.mail@kulla.me>
2025-02-13 23:59:16 +0100

    feat: Add integral_linear_form()

--- src/c_space.py
+++ src/c_space.py
@@ -6,9 +6,19 @@ using SymPy, a Python library for symbolic mathematics.
 
 import sympy as sp
 
+from functools import partial
+from sympy.abc import x, a, b
 
-def inner_product(f, g, a, b, x=sp.Symbol("x")):
+
+def inner_product(f, g, a=a, b=b, x=x):
     """
     Compute the inner product of two functions f and g in C([a,b]).
     """
     return sp.integrate(f * g, (x, a, b))
+
+
+def integral_linear_form(f, a=a, b=b, x=x):
+    """
+    Create a linear form based on the inner product with a fixed function f.
+    """
+    return partial(inner_product, f, a=a, b=b, x=x)
--- tests/c_space_test.py
+++ tests/c_space_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import sympy as sp
 
-from src.c_space import inner_product
+from src.c_space import inner_product, integral_linear_form
 
 
 class TestInnerProduct(unittest.TestCase):
@@ -12,6 +12,19 @@ class TestInnerProduct(unittest.TestCase):
         self.assertEqual(inner_product(x, x, 0, 1), sp.Rational(1, 3))
         self.assertEqual(inner_product(sp.sin(x), sp.sin(x), 0, sp.pi), sp.pi / 2)
         self.assertEqual(inner_product(sp.exp(x), sp.exp(x), 0, 1), (sp.exp(2) - 1) / 2)
+
+
+class TestIntegralLinearForm(unittest.TestCase):
+    def test_returns_linear_form(self):
+        x = sp.Symbol("x")
+
+        self.assertEqual(integral_linear_form(x, 0, 1)(x), sp.Rational(1, 3))
+        self.assertEqual(
+            integral_linear_form(sp.sin(x), 0, sp.pi)(sp.sin(x)), sp.pi / 2
+        )
+        self.assertEqual(
+            integral_linear_form(sp.exp(x), 0, 1)(sp.exp(x)), (sp.exp(2) - 1) / 2
+        )
 
 
 if __name__ == "__main__":
